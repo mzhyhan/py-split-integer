ef2320c0ce451718021511f8574f90b3598fe110
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-12-12 21:51:17 +0000

    tests for split_integer function that takes 2 positive integers value and number_of_parts and returns an array containing exactly number_of_parts integer elements

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,20 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    value = 9
+    parts = 3
+    result = split_integer(value, parts)
+    assert sum(result) == value
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
+    result = split_integer(12, 3)
+    assert result == [4, 4, 4]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    value = 10
+    result = split_integer(value, 1)
+    assert result == [value]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    result = split_integer(10, 3)
+    assert sorted(result) == [3, 3, 4]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+    result = split_integer(3, 5)
+    assert len(result) == 5
+    assert sum(result) == 3
+    assert 0 in result
